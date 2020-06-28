51be07af2312ae2f94163dd2d2d5570aff7052b8
Param Shah <paramshah09@gmail.com>
2020-06-29 05:25:37 +0530

    added CORS anywhere to fetch data and avoid CORS error

--- src/app/messages.service.ts
+++ src/app/messages.service.ts
@@ -9,7 +9,7 @@ export class MessagesService {
 
   constructor(private http: HttpClient) { }
 
-  private _url: string = 'https://x17hs8niwh.execute-api.us-east-1.amazonaws.com/dev/demo/get-seekers'
+  private _url: string = 'https://cors-anywhere.herokuapp.com/'+'https://x17hs8niwh.execute-api.us-east-1.amazonaws.com/dev/demo/get-seekers'
 
   fetchMessages(): Observable<any> {
     return this.http.get<any>(this._url);
